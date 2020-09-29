f587f80c23747e2e47e4b25f3476d77252b7d7d0
Mike van Niehoff <mikevanniehoff@example.com>
2020-09-29 15:32:44 +0200

    naam nu volledig werkend, niet gedaan met de String comparability

--- src/main/java/org/example/mike/overervenWerkNemers/app/domain/TijdelijkeWerknemer.java
+++ src/main/java/org/example/mike/overervenWerkNemers/app/domain/TijdelijkeWerknemer.java
@@ -1,6 +1,6 @@
 package org.example.mike.overervenWerkNemers.app.domain;
 
-public class TijdelijkeWerknemer extends Werknemer implements Comparable{
+public class TijdelijkeWerknemer extends Werknemer{
     private int aantalUren;
     private int uurloon;
 
@@ -14,36 +14,6 @@ public class TijdelijkeWerknemer extends Werknemer implements Comparable{
     {
         int salaris = aantalUren * uurloon;
         return salaris;
-    }
-
-    @Override
-    public int compareTo(Object otherObject) {
-        Werkneembaar otherWerknemer = (Werkneembaar) otherObject;
-        int letterDifference = 0;
-        int letterIndex = 0;
-        while((letterDifference == 0) && (letterIndex <= 15)) {
-            //zodat we geen exception krijgen van het vergelijken van kortere en langere namen
-            if(letterIndex < this.getNaam().length() && letterIndex < otherWerknemer.getNaam().length()){
-                //see if the letter at current index is different, starts at 0
-                letterDifference = this.getNaam().charAt(letterIndex) - otherWerknemer.getNaam().charAt(letterIndex);
-
-                //if not the same same give either negative or positive number to ordering
-                if(!(letterDifference == 0)){
-                    return letterDifference;}
-                //if same check next letter
-                else{
-                    letterIndex = letterIndex + 1;
-                }
-            }
-            else if((letterIndex <= this.getNaam().length()) && !(letterIndex <= otherWerknemer.getNaam().length())){
-                return -1; //kortere naam komt dan eerder te staan
-            }
-            else if(!(letterIndex <= this.getNaam().length()) && (letterIndex <= otherWerknemer.getNaam().length())){
-                return 1; //kortere naam komt dan eerder te staan
-            }
-            else{return 0;}
-        }
-        return letterDifference;
     }
 
     public String toStringRepresentatie() {
--- src/main/java/org/example/mike/overervenWerkNemers/app/domain/VasteWerknemer.java
+++ src/main/java/org/example/mike/overervenWerkNemers/app/domain/VasteWerknemer.java
@@ -1,6 +1,6 @@
 package org.example.mike.overervenWerkNemers.app.domain;
 
-public class VasteWerknemer extends Werknemer implements Comparable{
+public class VasteWerknemer extends Werknemer{
     private int salarisSchaal;
 
     public VasteWerknemer(int id, String naam, int salarisSchaal) {
@@ -12,36 +12,6 @@ public class VasteWerknemer extends Werknemer implements Comparable{
     {
         int salaris = salarisSchaal * 1000;
         return salaris;
-    }
-
-    @Override
-    public int compareTo(Object otherObject) {
-        Werkneembaar otherWerknemer = (Werkneembaar) otherObject;
-        int letterDifference = 0;
-        int letterIndex = 0;
-        while((letterDifference == 0) && (letterIndex <= 15)) {
-            //zodat we geen exception krijgen van het vergelijken van kortere en langere namen
-            if(letterIndex < this.getNaam().length() && letterIndex < otherWerknemer.getNaam().length()){
-                //see if the letter at current index is different, starts at 0
-                letterDifference = this.getNaam().charAt(letterIndex) - otherWerknemer.getNaam().charAt(letterIndex);
-
-                //if not the same same give either negative or positive number to ordering
-                if(!(letterDifference == 0)){
-                    return letterDifference;}
-                //if same check next letter
-                else{
-                    letterIndex = letterIndex + 1;
-                }
-            }
-            else if((letterIndex <= this.getNaam().length()) && !(letterIndex <= otherWerknemer.getNaam().length())){
-                return -1; //kortere naam komt dan eerder te staan
-            }
-            else if(!(letterIndex <= this.getNaam().length()) && (letterIndex <= otherWerknemer.getNaam().length())){
-                return 1; //kortere naam komt dan eerder te staan
-            }
-            else{return 0;}
-        }
-        return letterDifference;
     }
 
     public String toStringRepresentatie() {
